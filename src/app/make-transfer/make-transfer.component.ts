ef12e5422a9c8dfa0b9470b5250044e4882d73f7
Krupal Vasani <krupal.vasani@ecolab.com>
2021-06-29 14:05:51 +0530

    form validation

--- src/app/make-transfer/make-transfer.component.ts
+++ src/app/make-transfer/make-transfer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-make-transfer',
@@ -9,15 +10,21 @@ export class MakeTransferComponent implements OnInit {
   transection:any ;
   formData:any;
   forSumitted:boolean = false;
+  form = new FormGroup({});
   @Output() transectionObject: EventEmitter<object> = new EventEmitter();
 
   constructor() { }
   public amountvalue:number;
   public totalAmount:any;
+  public maxValue:any = 0;
   public acccountName:string ='';
 
   @Input() set totalBalance(value:any){
     this.totalAmount = value
+    let amount:any = parseFloat(this.totalAmount).toFixed(2);
+    this.maxValue = (amount - 500).toFixed(2);
+    //call the form controls
+    this.formControls();
   }
 
   ngOnInit(): void {
@@ -53,5 +60,22 @@ export class MakeTransferComponent implements OnInit {
 
   pushTheTransection(value:object){
     this.transectionObject.emit(value);
+  }
+
+  formControls(){
+    this.form = new FormGroup({
+      amount: new FormControl('', [Validators.required, Validators.min(0), Validators.max(this.maxValue - 500)]),
+      toAccount: new FormControl('', Validators.required),
+    });
+  }
+  
+  get f(){
+    debugger;
+    return this.form.controls;
+  }
+  
+  submit(){
+    debugger;
+    console.log(this.form.value);
   }
 }
